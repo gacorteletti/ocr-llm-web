a0a9d74099b79252621124fba4fba00c09d6c396
gacorteletti <gacorteletti@gmail.com>
2024-12-05 14:37:40 -0300

    passport js and jwt authentication

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { DocumentModule } from './document/document.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }), // include configuration globally
-    AuthModule, UserModule, DocumentModule,
+    ConfigModule.forRoot({ isGlobal: true }), // include globally
+    AuthModule,
+    UserModule,
+    DocumentModule,
+    PrismaModule,
   ],
 })
 export class AppModule {}
