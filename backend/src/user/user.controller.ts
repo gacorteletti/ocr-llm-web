3251d81d6e4263d18047877f3a75972aaa7d2f1c
gacorteletti <gacorteletti@gmail.com>
2024-12-05 15:11:21 -0300

    add custom guard

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { JwtGuard } from '../auth/guard/jwt.guard';
 
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get('studio') // users/stuio
   getMe(@Req() req: Request) {
     return req.user;
